5e98b4bb0fad02814fdb930a5c220b38f3f2518e
Bart S. <bart.schelpe@gmail.com>
2024-12-12 09:51:18 +0100

    Added class files to git

---
+++ CNetProjectAPI/Book.cs
@@ -0,0 +1,9 @@
+﻿namespace CNetProjectAPI;
+
+public class Book
+{
+    public string Title { get; set; }
+    public string Format { get; set; }
+    public string Author { get; set; }
+    public int Pages { get; set; }
+}
---
+++ CNetProjectAPI/Movie.cs
@@ -0,0 +1,9 @@
+﻿namespace CNetProjectAPI;
+
+public class Movie
+{
+    public string Title { get; set; }
+    public string Format { get; set; }
+    public string Director { get; set; }
+    public int ReleaseYear { get; set; }
+}
